fbcf6d0780fb2fea12749bb1e1e0fc2a1122f7b5
aoyama-micron <j.aoyama@micron-inc.co.jp>
2023-04-20 15:32:23 +0900

    ✨ ファイル追加

--- html/_head.php
+++ html/_head.php
@@ -6,4 +6,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>出会いサーチinclude</title>
 		<link rel="stylesheet" href="./assets/tailwind.css?<?php echo date('Ymdhis'); ?>" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&amp;family=Noto+Sans+JP:wght@400;500;700;900&amp;display=swap">
+		<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 	</head>
+	<body class="bg-gray-100">
+	<div class="mx-auto min-h-screen max-w-lg bg-white">
